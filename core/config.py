4d03e719475313e59fa7199e2f2949d2143ff3f4
YZ <yizetan1206@gmail.com>
2026-02-10 11:36:40 +0800

    test need undo

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_env: str = "local"
+    app_env: str = "llasdxzss"
     data_dir: str = "data/documents"
     openai_api_key: str | None = None
 
